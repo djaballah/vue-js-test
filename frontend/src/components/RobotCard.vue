201534e4214e7dfebfc4dc29f2e60ff260391391
Djaballah Mohammed Djedid <djaballah.djedid@outlook.com>
2022-05-07 21:47:57 +0100

    frontend: create basic RobotCard component
    - create basic RobotCard component that's going to be used
      in the Robots market listing.

---
+++ frontend/src/components/RobotCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  props: ["name", "imageLink", "price", "stock", "createdAt", "material"],
+  computed: {
+    formattedPrice() {
+      return new Intl.NumberFormat("th-TH", {
+        style: "currency",
+        currency: "LKR",
+      }).format(this.price);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="card" style="width: 18rem">
+    <img :src="this.imageLink" class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">{{ this.name }}</h5>
+      <ul>
+        <li>Price: {{ this.formattedPrice }}</li>
+        <li>Material: {{ this.material }}</li>
+        <li>Created at: {{ this.createdAt }}</li>
+        <li>{{ this.stock }} available</li>
+      </ul>
+      <div class="input-group mb-2">
+        <span class="input-group-text">Quantity</span>
+        <input type="number" class="form-control" min="1" :max="this.stock" />
+      </div>
+      <div class="row">
+        <div class="d-grid col">
+          <button class="btn btn-primary" type="button">Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
