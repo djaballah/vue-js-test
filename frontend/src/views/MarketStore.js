c98bcebbe4727ae6716818e6ebcae86bff318b63
Djaballah Mohammed Djedid <djaballah.djedid@outlook.com>
2022-05-08 22:10:26 +0100

    frontend/Cart: limit selected robot's size to 5.
    - Don't allow buying more than 5 different robots.

--- frontend/src/views/MarketStore.js
+++ frontend/src/views/MarketStore.js
@@ -6,6 +6,14 @@ export default class Store {
   }
 
   addRobotToCart(robot, amount) {
+    if (
+      this.state.selectedRobots.size == 5 &&
+      !this.state.selectedRobots.has(robot.name)
+    ) {
+      alert("You can't buy more than different 5 robots");
+      return;
+    }
+
     let oldAmount = 0;
     if (this.state.selectedRobots.has(robot.name)) {
       oldAmount = this.state.selectedRobots.get(robot.name).amount;
